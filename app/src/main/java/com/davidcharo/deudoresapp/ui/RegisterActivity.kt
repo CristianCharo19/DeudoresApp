9a7e478c86ea1a026bdc630746f5f1088df84485
Cristian David Charo <cristian.charo@udea.edu.co>
2021-08-31 08:10:37 -0500

    feature/ Creación RegisterActivity

---
+++ app/src/main/java/com/davidcharo/deudoresapp/ui/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.davidcharo.deudoresapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
